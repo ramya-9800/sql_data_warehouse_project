4c742e0291bcbc720184da691523eaea47f4af33
ramya-9800 <ramya.inbox01@gmail.com>
2026-08-20 12:01:48 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,13 @@
+/*
+Create Database and Schemas
 
+Script purpose:
+    This script creates a new database named "Data warehouse".
+    Additionally, the script sets up three schemas within the database: bronze, silver, gold.
+*/
+
+create  database dataWarehouse;
+use datawarehouse;
+create schema bronze;
+create schema silver;
+create schema gold;
